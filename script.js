bbedfe0a1a34b0d42d58662a0095f27662705317
notmihnea1 <notmihnea1@proton.me>
2026-07-22 19:22:30 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const buton = document.getElementById("salutbutton");
+const mesaj = document.getElementById("mesaj");
+
+buton.addEventListener("click", function () {
+    mesaj.textContent = "nimic de vazut aici";
+});
