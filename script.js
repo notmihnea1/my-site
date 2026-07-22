92a86d2f3d0a56f121385d9b07176eab2c002329
notmihnea1 <notmihnea1@proton.me>
2026-07-22 22:04:41 +0300

    Redesigned homepage and added construction notice

--- script.js
+++
@@ -1,6 +0,0 @@
-const buton = document.getElementById("salutbutton");
-const mesaj = document.getElementById("mesaj");
-
-buton.addEventListener("click", function () {
-    mesaj.textContent = "nimic de vazut aici";
-});
